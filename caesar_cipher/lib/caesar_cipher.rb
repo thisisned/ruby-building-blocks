6de24b42d22ae82e35416c223dfcfe833ae153b7
Ned Wilson Eames <ned.wilson.eames@gmail.com>
2017-06-06 19:48:27 +0100

    Clean up formatting

--- caesar_cipher/lib/caesar_cipher.rb
+++ caesar_cipher/lib/caesar_cipher.rb
@@ -1,20 +1,20 @@
 def caesar_cipher(string, shift = 1) # Shift positive for rightwards, negative for leftwards
-	shift = shift % 26 # Any shift over 26 gets modulo'd out
-	coded_array = []
-	string.scan(/./) do |char|
-		new_ord = (char.ord + shift)
-		if char =~ /[A-Z]/ # Regex: if the character is a captial letter
-			unless (65..90).include?(new_ord)
-				new_ord -= 26
-			end
-			coded_array << new_ord.chr
-		elsif char =~ /[a-z]/ # Regex: if the character is a lower-case letter
-			unless (97..122).include?(new_ord)
-				new_ord -= 26
-			end
-			coded_array << new_ord.chr
-		else coded_array << char
-		end
-	end
-	p coded_array.join
+  shift = shift % 26 # Any shift over 26 gets modulo'd out
+  coded_array = []
+  string.scan(/./) do |char|
+    new_ord = (char.ord + shift)
+    if char =~ /[A-Z]/ # Regex: if the character is a captial letter
+      unless (65..90).include?(new_ord)
+        new_ord -= 26
+      end
+      coded_array << new_ord.chr
+    elsif char =~ /[a-z]/ # Regex: if the character is a lower-case letter
+      unless (97..122).include?(new_ord)
+        new_ord -= 26
+      end
+      coded_array << new_ord.chr
+    else coded_array << char
+    end
+  end
+  p coded_array.join
 end
